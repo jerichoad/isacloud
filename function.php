0ecf1233c55fe649e3bf3a3bb1101b0db96865a2
jerichoad <62791330+jerichoad@users.noreply.github.com>
2020-03-28 20:13:42 +0700

    Add files via upload

--- function.php
+++ function.php
@@ -1,15 +1,9 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
-$conn = mysqli_connect($dbhost,'root','','jerichodb');
-
-function query($query){
-    global $conn;
-    $result = mysqli_query($conn,$query);
-    $rows = [];
-    while($row = mysqli_fetch_assoc($result)){
-        $rows[] = $row;
-    }
-    return $rows;
+$conn = new mysqli($dbhost,'root','ubaya','jerichodb');
+if ($c->connect_error) {
+    echo "Unable to connect, please try again";
+    die();
 }
 
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
-require 'function.php';
+require_once("function.php");
 $mahasiswa = query("SELECT * FROM mahasiswa");
+$result = $c
 
 ?>
 <!DOCTYPE html>
@@ -11,25 +12,34 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
 </head>
 <body>
     <h1>Daftar mahasiswa</h1>
-    
+
     <table border="1" cellpadding="10" cellspacing = "0">
-        <tr>
-            <th>No.</th>
+        <tr>            
             <th>NRP</th>
             <th>Nama</th>
             <th>Jurusan</th>
         </tr>
 
-        <?php $i=1; ?>
-        <?php foreach($mahasiswa as $row) : ?>
-        <tr>
-            <td><?= $i; ?></td>
-            <td><?= $row["nrp"]; ?></td>
-            <td><?= $row["nama"]; ?></td>
-            <td><?= $row["jurusan"]; ?></td>
-        </tr>
-        <?php $i++ ?>
-        <?php endforeach; ?>
+        <?php 
+        $sql = "SELECT * FROM mahasiswa";
+        $result = $c->query($sql);
+        if ($result->num_rows > 0) {
+    
+          $drivers = array();
+          $i = 0;
+
+            while ($obj = $result->fetch_assoc()) {
+                echo '<tr>';
+                echo '<td>' . $obj['nrp'] . '</td>';
+                echo '<td>' . $obj['nama'] . '</td>';
+                echo '<td>' . $obj['jurusan'] . '</td>';
+            }
+        } else {
+          echo "Empty table.";
+          die();
+        } 
+        ?>
+       
     </table>
 </body>
 </html>
